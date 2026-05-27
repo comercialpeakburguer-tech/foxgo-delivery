f32cd0a619222cd74411e16893dcbc054093e5d6
comercialpeakburguer-tech <comercialpeakburguer@gmail.com>
2026-05-27 00:17:25 -0300

    fix: point baseUrl to admin.foxgodelivery.com.br (real 6ammart backend) + fix webHostedUrl typo

--- flutter-app/lib/util/app_constants.dart
+++ flutter-app/lib/util/app_constants.dart
@@ -10,13 +10,13 @@ class AppConstants {
   static const String fontFamily = 'Roboto';
   static const bool payInWevView = false;
   static const int balanceInputLen = 10;
-  static const String webHostedUrl = 'https://foxgodelivey.com.br';
+  static const String webHostedUrl = 'https://www.foxgodelivery.com.br';
   static const bool useReactWebsite = false;
   static const bool stopPolylineAnimation = false;
   static const String googleServerClientId = '491987943015-agln6biv84krpnngdphj87jkko7r9lb8.apps.googleusercontent.com';
   static const String pusherBroadcustUrl = '/api/v1/broadcasting/user-auth';
 
-  static const String baseUrl = 'https://foxgodelivey.com.br';
+  static const String baseUrl = 'https://admin.foxgodelivery.com.br';
   static const String categoryUri = '/api/v1/categories';
   static const String bannerUri = '/api/v1/banners';
   static const String storeItemUri = '/api/v1/items/latest';
